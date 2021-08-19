c4dfd09a539e8f35dca780a30020529301e30709
Julio Carrió <julio.carrio@gmail.com>
2021-08-19 23:55:09 +0000

    Aula 19/08/2021

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect,\
+    abort, render_template, flash
+
+# configuração
+DATABASE = "blog.db"
+SECRET_KEY = "pudim"  # chave de criptografia usada pelo flask, não deveria ser salvo no código
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd() 
+
+@app.teardown_request
+def depois_requisicao(exc):
+    g.bd.close()    
+
+@app.route('/')
+def exibir_entradas():
+    return render_template('exibir_entradas.html')
 
 @app.route('/hello')
 def pagina_inicial():
-    return "Helo World"
+    return "<h1 style='color: green;'>Hello World</h1>"
